f27765acfb54d3060472eb9e2033c40dd0b523ac
MrSparkzz <xmrsparkzz@gmail.com>
2014-10-02 10:10:24 -0400

    Started work on variables and added comments to help the reader with understanding

--- Main.py
+++ Main.py
@@ -1,3 +1,34 @@
 __author__ = 'Brendon'
 
-print "Hello, Python!";
+
+#########################
+# Basic Printing        #
+#########################
+
+print "Hello, Python!"; # prints 'Hello, Python!' to the console
+
+
+#########################
+# Variables             #
+#########################
+print ""
+
+counter = 100       # integer assignment
+miles   = 1000.0    # a floating point value
+name    = "John"    # a string
+
+print counter   # prints the counter variable
+print miles     # prints the miles variable
+print name      # prints the name variable
+
+a = b = c = 1   # multiple assignment; allows you to assign a value to multiple variables at once
+
+print a # prints the a variable
+print b # prints the b variable
+print c # prints the c variable
+
+a, b, c = 1, 2, name    # assigns 1 to 'a', 2 to 'b' and the name variable to the c variable
+
+print a # prints the a variable
+print b # prints the b variable
+print c # prints the c variable
